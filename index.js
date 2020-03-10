243cf7b9538867278f6026f8dbed0b80e196db69
adit313 <app2116@gmail.com>
2020-03-10 11:35:17 -0400

    Done.

--- index.js
+++ index.js
@@ -5,6 +5,25 @@ function handleClick(e) {
   displayMessage(greet(timeString))
 }
 
+function greet(timeString) {
+let hour = parseInt(timeString.split(0,2))
+if (hour <12) {
+  return "Good Morning"
+} else {
+  if (hour <17) {
+    return "Good Afternoon"
+  } else {
+    return "Good Evening"
+  }
+}
+}
+
+function displayMessage(message) {
+  const greeting = document.getElementById('greeting')
+  // let textnode = document.createTextNode(message);
+  greeting.innerText = message
+}
+
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
