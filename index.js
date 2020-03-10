1176d4d0d7818880127bcb114ba5cc2f61bf8ad0
haltonba <haltonba94@gmail.com>
2020-03-10 13:33:02 -0400

    Done.

--- index.js
+++ index.js
@@ -9,3 +9,20 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function greet(time) {
+  let timeInteger = time.replace(":", "");
+  if (timeInteger < 1200) {
+    return "Good Morning";
+  } 
+  else if (1200 <= timeInteger && timeInteger <= 1700){
+    return "Good Afternoon";
+  }
+  else {
+    return "Good Evening";
+  }
+}
+
+function displayMessage(string) {
+  document.getElementById('greeting').innerText = string;
+}
