16cb12ad06f37244e6bfe7016bb64a5487410b8c
github username <github email address>
2020-03-10 11:54:24 -0400

    Done.

--- index.js
+++ index.js
@@ -12,13 +12,12 @@ function handleClick(e) {
 
 function greet(timeString){
   const hour = parseInt(timeString, 10)
-  if (hour < 12){
-    return "Good Morning"
-  } else if (hour > 12 && hour < 17) {
+  if (hour < 12) { 
+    return "Good Morning" 
+  } else if (hour >= 12 && hour < 17) { 
     return "Good Afternoon"
   } else {
-    return "Good Evening"
-  }
+    return "Good Evening"}
 }
 
 function displayMessage(greets){
